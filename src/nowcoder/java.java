fdbcdf589f90f4c4636bd3f437e48559e2f80f8e
onnsttt <739503629@qq.com>
2018-09-06 22:37:49 +0800

    2018年9月6日22:37:39

--- src/nowcoder/java.java
+++ src/nowcoder/java.java
@@ -1,26 +1,38 @@
 package nowcoder;
-
 import java.util.Scanner;
 
 public class java {
     public static void main(String[] args) {
-
         Scanner cin = new Scanner(System.in);
-
-        StringBuffer string = new StringBuffer(cin.next());
-
-        int n = cin.nextInt();
-        int l,r;
-        String tmp = "";
-        for(int i = 0;i<n;i++){
-            l = cin.nextInt();
-            r = cin.nextInt();
-            tmp ="";
-            for(int j = r+l-1;j>=l;j--){
-                tmp+=string.charAt(j);
-            }
-            string.insert(l+r,tmp);
-        }
-        System.out.println(string);
+       int a[] = new int[100005];
+       int n,k,t;
+       n = cin.nextInt();
+       k = cin.nextInt();
+       t = cin.nextInt();
+       for(int i = 1;i<=n;i++){
+           a[i] = cin.nextInt();
+       }
+       int ans = 0;
+       int num[] = new int[100005];
+       for(int i = 1;i<=k;i++){
+            num[a[i]]++;
+       }
+       for(int i = 1;i<=k;i++){
+           if(num[a[i]]>=t){
+               ans++;
+               break;
+           }
+       }
+       for(int i = 2;i<=n+1-k;i++){
+           num[a[i-1]]--;
+           num[a[k+i-1]]++;
+           for(int j = i;j<=k+i-1;j++){
+               if(num[a[j]]>=t){
+                   ans++;
+                   break;
+               }
+           }
+       }
+        System.out.println(ans);
     }
 }
